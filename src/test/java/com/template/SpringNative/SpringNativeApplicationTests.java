7d958896b51eed5d9097095a1099ad9e7e0a056f
Francesco De Domenico <dedomenico.francesco@outlook.com>
2022-08-11 10:51:42 +0200

    first release

---
+++ src/test/java/com/template/SpringNative/SpringNativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.template.SpringNative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
